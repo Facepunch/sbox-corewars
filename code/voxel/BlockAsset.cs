c745593081df1a2b57b2f6ccada67deb60b04835
kurozael <kurozael@gmail.com>
2022-02-01 08:16:56 +0000

    Added basic BlockAsset class with just FriendlyName and TextureId for now

---
+++ code/voxel/BlockAsset.cs
@@ -0,0 +1,11 @@
+﻿using Sandbox;
+
+namespace Facepunch.CoreWars.Voxel
+{
+	[Library( "block" ), AutoGenerate]
+	public class BlockAsset : Asset
+	{
+		[Property] public string FriendlyName { get; set; }
+		[Property] public byte TextureId { get; set; }
+	}
+}
