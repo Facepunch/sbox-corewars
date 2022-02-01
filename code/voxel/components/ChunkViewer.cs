57983645419bc7e11e2a70001337131cebfcab31
kurozael <kurozael@gmail.com>
2022-02-01 07:09:16 +0000

    Added stub ChunkViewer component (will eventually handle loading of chunks)

---
+++ code/voxel/components/ChunkViewer.cs
@@ -0,0 +1,19 @@
+﻿using Sandbox;
+using System.Collections.Generic;
+
+namespace Facepunch.CoreWars.Voxel
+{
+	public partial class ChunkViewer : EntityComponent
+	{
+		[Net] public IList<Chunk> LoadedChunks { get; private set; }
+
+		protected override void OnActivate()
+		{
+			LoadedChunks = new List<Chunk>();
+
+			base.OnActivate();
+		}
+
+		// TODO: Our RPCs for receiving chunks will go here, but entity components cannot have RPCs yet.
+	}
+}
