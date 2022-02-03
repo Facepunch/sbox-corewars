5cac6d004bbc054df84c0d738289a7dec410eaa3
bakscratch <lt_8@hotmail.co.uk>
2022-02-03 20:03:25 +0000

    Addition blocks

---
+++ code/blocks/MetalplateredBlock.cs
@@ -0,0 +1,12 @@
+﻿using Facepunch.CoreWars.Voxel;
+using Sandbox;
+
+namespace Facepunch.CoreWars.Blocks
+{
+	[Library]
+	public class MetalplateredBlock : BlockType
+	{
+		public override string DefaultTexture => "metalplatered";
+		public override string FriendlyName => "Metal Plate Red";
+	}
+}
