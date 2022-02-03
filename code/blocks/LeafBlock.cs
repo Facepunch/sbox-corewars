b14f0fa24c483243a230f37d0a17bf08a6f0d570
bakscratch <lt_8@hotmail.co.uk>
2022-02-03 00:20:32 +0000

    Correct name

--- code/blocks/LeafBlock.cs
+++ code/blocks/LeafBlock.cs
@@ -6,7 +6,7 @@ namespace Facepunch.CoreWars.Blocks
 	[Library]
 	public class LeafBlock : BlockType
 	{
-		public override string FriendlyName => "Glass";
+		public override string FriendlyName => "Leaf";
 		public override bool IsTranslucent => true;
 		public override byte TextureId => 9;
 		public override byte BlockId => 7;
