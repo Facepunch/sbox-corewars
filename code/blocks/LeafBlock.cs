3fa10fcd5029578688d27be011efc12e57cdd935
bakscratch <lt_8@hotmail.co.uk>
2022-02-03 00:06:31 +0000

    Added new block

---
+++ code/blocks/LeafBlock.cs
@@ -0,0 +1,14 @@
+﻿using Facepunch.CoreWars.Voxel;
+using Sandbox;
+
+namespace Facepunch.CoreWars.Blocks
+{
+	[Library]
+	public class LeafBlock : BlockType
+	{
+		public override string FriendlyName => "Glass";
+		public override bool IsTranslucent => true;
+		public override byte TextureId => 9;
+		public override byte BlockId => 7;
+	}
+}
