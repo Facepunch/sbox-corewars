bc9305568138f6f02634dcdcce5d4b37204f2686
bakscratch <lt_8@hotmail.co.uk>
2022-02-03 11:29:49 +0000

    Addition blocks

---
+++ code/blocks/ClothBlock.cs
@@ -0,0 +1,12 @@
+﻿using Facepunch.CoreWars.Voxel;
+using Sandbox;
+
+namespace Facepunch.CoreWars.Blocks
+{
+	[Library]
+	public class ClothBlock : BlockType
+	{
+		public override string DefaultTexture => "cloth";
+		public override string FriendlyName => "Cloth";
+	}
+}
---
+++ code/blocks/GravelBlock.cs
@@ -0,0 +1,12 @@
+﻿using Facepunch.CoreWars.Voxel;
+using Sandbox;
+
+namespace Facepunch.CoreWars.Blocks
+{
+	[Library]
+	public class GravelBlock : BlockType
+	{
+		public override string DefaultTexture => "gravel";
+		public override string FriendlyName => "Gravel";
+	}
+}
