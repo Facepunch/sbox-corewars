eabec74fa90a6bc78d06300124f5a5209ab5b40b
bakscratch <lt_8@hotmail.co.uk>
2022-02-04 16:00:54 +0000

    Added additional blocks

---
+++ code/blocks/StonebrickBlock.cs
@@ -0,0 +1,12 @@
+﻿using Facepunch.CoreWars.Voxel;
+using Sandbox;
+
+namespace Facepunch.CoreWars.Blocks
+{
+	[Library]
+	public class StonebrickBlock : BlockType
+	{
+		public override string DefaultTexture => "stonebrick";
+		public override string FriendlyName => "Stone Brick";
+	}
+}
