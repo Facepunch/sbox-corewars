3d74599d72b87d6814fb7da1c4e688a3a43a8b1f
kurozael <kurozael@gmail.com>
2022-02-07 16:50:35 +0000

    Start fleshing out an Inventory API

---
+++ code/inventory/Inventory.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace Facepunch.CoreWars.Inventory
+{
+	public static class Inventory
+	{
+		private static Dictionary<uint, InventoryContainer> Containers { get; set; } = new();
+		private static Dictionary<uint, InventoryItem> Items { get; set; } = new();
+
+		private static uint NextContainerId { get; set; }
+		private static uint NextItemId { get; set; }
+
+		public static InventoryContainer CreateContainer()
+		{
+			return CreateContainer( NextContainerId++ );
+		}
+
+		public static InventoryContainer CreateContainer( uint id )
+		{
+			if ( Containers.TryGetValue( id, out var container ) )
+			{
+				return container;
+			}
+
+			container = new InventoryContainer( id );
+			Containers.Add( id, container );
+			return container;
+		}
+	}
+}
---
+++ code/inventory/InventoryContainer.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+
+namespace Facepunch.CoreWars.Inventory
+{
+	public class InventoryContainer
+	{
+		public uint ContainerId { get; private set; }
+
+		public InventoryContainer( uint id )
+		{
+			ContainerId = id;
+		}
+
+		public virtual void Serialize( BinaryWriter writer )
+		{
+
+		}
+
+		public virtual void Deserialize( BinaryReader reader )
+		{
+
+		}
+	}
+}
