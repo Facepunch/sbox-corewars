8da64b84485905edb2bea5d8524fbac584dfe368
kurozael <kurozael@gmail.com>
2022-03-02 05:27:24 +0000

    Added Save World and Load World options to the radial menu (only Save works right now)

---
+++ code/editor/tools/AreaBlocksTool.cs
@@ -0,0 +1,12 @@
+﻿using Facepunch.CoreWars.Blocks;
+using Facepunch.Voxels;
+using Sandbox;
+
+namespace Facepunch.CoreWars.Editor
+{
+	[EditorToolLibrary( Title = "Area Blocks", Description = "Create blocks in a defined areas", Icon = "textures/ui/tools/areablocks.png" )]
+	public class AreaBlocksTool : EditorTool
+	{
+		
+	}
+}
---
+++ code/editor/tools/BlockDataTool.cs
@@ -0,0 +1,12 @@
+﻿using Facepunch.CoreWars.Blocks;
+using Facepunch.Voxels;
+using Sandbox;
+
+namespace Facepunch.CoreWars.Editor
+{
+	[EditorToolLibrary( Title = "Block Data", Description = "Edit the data of a block", Icon = "textures/ui/tools/blockdata.png" )]
+	public class BlockDataTool : EditorTool
+	{
+		
+	}
+}
