c4eae2382ff0556b937cd6efeed6814c60d9eab7
kurozael <kurozael@gmail.com>
2022-03-02 05:28:05 +0000

    Typo

--- code/editor/tools/AreaBlocksTool.cs
+++ code/editor/tools/AreaBlocksTool.cs
@@ -4,7 +4,7 @@ using Sandbox;
 
 namespace Facepunch.CoreWars.Editor
 {
-	[EditorToolLibrary( Title = "Area Blocks", Description = "Create blocks in a defined areas", Icon = "textures/ui/tools/areablocks.png" )]
+	[EditorToolLibrary( Title = "Area Blocks", Description = "Create blocks in a defined area", Icon = "textures/ui/tools/areablocks.png" )]
 	public class AreaBlocksTool : EditorTool
 	{
 		
