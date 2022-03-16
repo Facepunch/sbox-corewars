bc0b4da4a3eb46f83a1884c26dfd16e571510f30
kurozael <kurozael@gmail.com>
2022-03-16 15:36:10 +0000

    Added VoxelWorldLoader entity to Core Wars for Hammer

---
+++ code/entities/hammer/VoxelWorldLoader.cs
@@ -0,0 +1,12 @@
+﻿using Sandbox;
+
+namespace Facepunch.CoreWars
+{
+	[Library( "cw_voxel_world_loader" )]
+	[Hammer.EntityTool( "Voxel World Loader", "Core Wars" )]
+	[Hammer.EditorSprite( "editor/rts_minimap.vmat" )]
+	public class VoxelWorldLoader : ModelEntity
+	{
+		[Property] public string FileName { get; set; }
+	}
+}
