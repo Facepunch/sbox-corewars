c06c34b96d14d15af1293dc6ecf3502a7f89a4d6
kurozael <kurozael@gmail.com>
2022-03-16 20:01:19 +0000

    Added crude Editor Save Dialog and Editor Load Dialog

---
+++ code/ui/editor/EditorLoadDialog.cs
@@ -0,0 +1,56 @@
+﻿using Facepunch.Voxels;
+using Sandbox;
+using Sandbox.UI;
+using System;
+
+namespace Facepunch.CoreWars.Editor
+{
+	[UseTemplate]
+	public partial class EditorLoadDialog : Panel
+	{
+		private static EditorLoadDialog Current { get; set; }
+
+		public TextEntry Input { get; set; }
+
+		public static void Open()
+		{
+			Current?.Delete();
+			Current = new EditorLoadDialog();
+
+			Game.Hud.AddChild( Current );
+		}
+
+		public override void Tick()
+		{
+			if ( !VoxelWorld.Current.IsValid() ) return;
+
+			base.Tick();
+		}
+
+		protected virtual void HandleSubmit()
+		{
+			Game.LoadEditorMapCmd( Input.Text );
+		}
+
+		protected virtual void HandleClose()
+		{
+			Delete();
+		}
+
+		protected override void PostTemplateApplied()
+		{
+			var state = Game.GetStateAs<EditorState>();
+
+			if ( !string.IsNullOrEmpty( state.CurrentFileName ) )
+			{
+				Input.Text = state.CurrentFileName;
+			}
+
+			Input.Focus();
+			Input.AddEventListener( "onsubmit", () => HandleSubmit() );
+			Input.AddEventListener( "onblur", () => HandleClose() );
+
+			base.PostTemplateApplied();
+		}
+	}
+}
---
+++ code/ui/editor/EditorSaveDialog.cs
@@ -0,0 +1,56 @@
+﻿using Facepunch.Voxels;
+using Sandbox;
+using Sandbox.UI;
+using System;
+
+namespace Facepunch.CoreWars.Editor
+{
+	[UseTemplate]
+	public partial class EditorSaveDialog : Panel
+	{
+		private static EditorSaveDialog Current { get; set; }
+
+		public TextEntry Input { get; set; }
+
+		public static void Open()
+		{
+			Current?.Delete();
+			Current = new EditorSaveDialog();
+
+			Game.Hud.AddChild( Current );
+		}
+
+		public override void Tick()
+		{
+			if ( !VoxelWorld.Current.IsValid() ) return;
+
+			base.Tick();
+		}
+
+		protected virtual void HandleSubmit()
+		{
+			Game.SaveEditorMapCmd( Input.Text );
+		}
+
+		protected virtual void HandleClose()
+		{
+			Delete();
+		}
+
+		protected override void PostTemplateApplied()
+		{
+			var state = Game.GetStateAs<EditorState>();
+
+			if ( !string.IsNullOrEmpty( state.CurrentFileName ) )
+			{
+				Input.Text = state.CurrentFileName;
+			}
+
+			Input.Focus();
+			Input.AddEventListener( "onsubmit", () => HandleSubmit() );
+			Input.AddEventListener( "onblur", () => HandleClose() );
+
+			base.PostTemplateApplied();
+		}
+	}
+}
