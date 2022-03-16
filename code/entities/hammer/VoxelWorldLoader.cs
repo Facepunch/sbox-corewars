637d5f0209c0e02cfaba044f20ea21c45976a6c6
bakscratch <lt_8@hotmail.co.uk>
2022-03-16 19:54:03 +0000

    Voxel World Loader icon

--- code/entities/hammer/VoxelWorldLoader.cs
+++ code/entities/hammer/VoxelWorldLoader.cs
@@ -4,7 +4,7 @@ namespace Facepunch.CoreWars
 {
 	[Library( "cw_voxel_world_loader" )]
 	[Hammer.EntityTool( "Voxel World Loader", "Core Wars" )]
-	[Hammer.EditorSprite( "editor/rts_minimap.vmat" )]
+	[Hammer.EditorSprite( "materials/editor/voxel_world_loader.vmat" )]
 	public class VoxelWorldLoader : ModelEntity
 	{
 		[Property] public string FileName { get; set; }
