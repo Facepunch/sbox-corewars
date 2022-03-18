a999947dcadf6aeffda3183bfe329b8c759c2890
kurozael <kurozael@gmail.com>
2022-03-18 07:49:45 +0000

    Add AutoCompleteInput and AutoCompleteList classes

---
+++ code/ui/editor/AutoCompleteInput.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Sandbox;
+using Sandbox.UI;
+
+namespace Facepunch.CoreWars.Editor
+{
+	public interface IAutoCompleteList
+	{
+		void ClearOptions();
+		void AddOption( string option, Action callback );
+	}
+
+	public class AutoCompleteInput : TextEntry
+	{
+		public IAutoCompleteList AutoCompleteList { get; set; }
+		public Func<string, string[]> AutoCompleteHandler { get; set; }
+
+		public void SetAutoCompleteList( IAutoCompleteList list )
+		{
+			AutoCompleteList = list;
+		}
+
+		public override void OnValueChanged()
+		{
+			base.OnValueChanged();
+
+			UpdateAutoCompleteList();
+		}
+
+		public void UpdateAutoCompleteList()
+		{
+			UpdateAutoCompleteList( Text );
+		}
+
+		public void UpdateAutoCompleteList( string partial )
+		{
+			var results = AutoCompleteHandler?.Invoke( partial );
+
+			AutoCompleteList.ClearOptions();
+
+			if ( results == null ) return;
+
+			for ( int i = 0; i < results.Length; i++ )
+			{
+				var result = results[i];
+				AutoCompleteList.AddOption( result, () =>
+				{
+					Text = result;
+					CaretPosition = TextLength;
+					UpdateAutoCompleteList();
+				} );
+			}
+		}
+	}
+}
---
+++ code/ui/editor/AutoCompleteList.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Sandbox;
+using Sandbox.UI;
+
+namespace Facepunch.CoreWars.Editor
+{
+	[UseTemplate]
+	public class AutoCompleteList : Panel, IAutoCompleteList
+	{
+		public void ClearOptions()
+		{
+			DeleteChildren();
+		}
+
+		public void AddOption( string option, Action callback )
+		{
+			var item = AddChild<Button>( "option" );
+			item.AddEventListener( "onclick", callback );
+			item.SetText( option );
+		}
+	}
+}
