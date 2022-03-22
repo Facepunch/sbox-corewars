6b901ac58226bc10da1f4af2055bb17024dca7ce
kurozael <kurozael@gmail.com>
2022-03-22 17:01:33 +0000

    Added stub for Entities Tool in the radial menu

---
+++ code/editor/tools/EntitiesTool.cs
@@ -0,0 +1,54 @@
+﻿using Facepunch.Voxels;
+using Sandbox;
+
+namespace Facepunch.CoreWars.Editor
+{
+	[EditorToolLibrary( Title = "Entities", Description = "Add or manipulate entities", Icon = "textures/ui/tools/entities.png" )]
+	public class EntitiesTool : EditorTool
+	{
+		public override void Simulate( Client client )
+		{
+			var currentMap = VoxelWorld.Current;
+
+			if ( IsClient && currentMap.IsValid() )
+			{
+				var aimVoxelPosition = GetAimVoxelPosition( 4f );
+				var aimSourcePosition = VoxelWorld.Current.ToSourcePosition( aimVoxelPosition );
+			}
+
+			base.Simulate( client );
+		}
+
+		public override void OnSelected()
+		{
+			if ( IsClient )
+			{
+				
+			}
+		}
+
+		public override void OnDeselected()
+		{
+			if ( IsClient )
+			{
+				
+			}
+		}
+
+		protected override void OnPrimary( Client client )
+		{
+			if ( IsServer )
+			{
+				
+			}
+		}
+
+		protected override void OnSecondary( Client client )
+		{
+			if ( IsServer )
+			{
+				
+			}
+		}
+	}
+}
