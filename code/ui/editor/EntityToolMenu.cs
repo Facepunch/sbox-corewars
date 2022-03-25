0d2bcb6354e84be7b39b28c76c3caa0f7a908310
kurozael <kurozael@gmail.com>
2022-03-25 12:03:43 +0000

    Add ToolSelectorMenu and EntityToolMenu seperate radial menus

---
+++ code/ui/editor/EntityToolMenu.cs
@@ -0,0 +1,23 @@
+﻿using Facepunch.CoreWars.Editor;
+using Facepunch.CoreWars.Inventory;
+using Facepunch.Voxels;
+using Sandbox;
+using Sandbox.UI;
+using System;
+using System.Collections.Generic;
+
+namespace Facepunch.CoreWars.Editor
+{
+	[UseTemplate]
+	public partial class EntityToolMenu : SimpleRadialMenu
+	{
+		public override InputButton Button => InputButton.Reload;
+
+		public override void Populate()
+		{
+			AddAction( "Test", "Test", "textures/ui/blocklist.png", () => Log.Info( "Test" ) );
+
+			base.Populate();
+		}
+	}
+}
---
+++ code/ui/editor/ToolSelectorMenu.cs
@@ -0,0 +1,36 @@
+﻿using Facepunch.CoreWars.Editor;
+using Facepunch.CoreWars.Inventory;
+using Facepunch.Voxels;
+using Sandbox;
+using Sandbox.UI;
+using System;
+using System.Collections.Generic;
+
+namespace Facepunch.CoreWars.Editor
+{
+	[UseTemplate]
+	public partial class ToolSelectorMenu : SimpleRadialMenu
+	{
+		public override InputButton Button => InputButton.Score;
+
+		public override void Populate()
+		{
+			var available = Library.GetAttributes<EditorToolLibraryAttribute>();
+
+			foreach ( var attribute in available )
+			{
+				AddTool( attribute );
+			}
+
+			AddAction( "Block List", "View available blocks", "textures/ui/blocklist.png", () => EditorBlockList.Open() );
+
+			if ( Local.Client.IsListenServerHost )
+			{
+				AddAction( "Save World", "Save world to disk", "textures/ui/save.png", () => EditorSaveDialog.Open() );
+				AddAction( "Load World", "Load world from disk", "textures/ui/load.png", () => EditorLoadDialog.Open() );
+			}
+
+			base.Populate();
+		}
+	}
+}
