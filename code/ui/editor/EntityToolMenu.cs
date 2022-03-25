154dde94aab6b0f4379f08ed19c305eaee03df4d
kurozael <kurozael@gmail.com>
2022-03-25 12:16:41 +0000

    Add ChangeModeCmd / SetMode and change callback for Mode to EntitiesTool

--- code/ui/editor/EntityToolMenu.cs
+++ code/ui/editor/EntityToolMenu.cs
@@ -15,9 +15,28 @@ namespace Facepunch.CoreWars.Editor
 
 		public override void Populate()
 		{
-			AddAction( "Test", "Test", "textures/ui/blocklist.png", () => Log.Info( "Test" ) );
+			AddAction( "Place", "Place an entity in the world", "textures/ui/blocklist.png", () => SetToolMode( EntitiesToolMode.Place ) );
+			AddAction( "Edit Data", "Edit the data of an entity", "textures/ui/blocklist.png", () => SetToolMode( EntitiesToolMode.DataEditor ) );
+			AddAction( "Remove", "Remove an entity", "textures/ui/blocklist.png", () => SetToolMode( EntitiesToolMode.Remove ) );
+			AddAction( "Move/Rotate", "Move or rotate an entity", "textures/ui/blocklist.png", () => SetToolMode( EntitiesToolMode.MoveAndRotate ) );
 
 			base.Populate();
+		}
+
+		protected override bool ShouldOpen()
+		{
+			if ( Local.Pawn is not EditorPlayer player )
+				return false;
+
+			return player.Tool is EntitiesTool;
+		}
+
+		private void SetToolMode( EntitiesToolMode mode )
+		{
+			if ( Local.Pawn is not EditorPlayer player )
+				return;
+
+			EntitiesTool.ChangeModeCmd( (int)mode );
 		}
 	}
 }
