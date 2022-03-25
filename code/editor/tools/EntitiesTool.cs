f1c0fe2127725ab9b94a9b11bfc482875f536eac
kurozael <kurozael@gmail.com>
2022-03-25 12:37:26 +0000

    Add ChangeLibraryAttributeCmd and support for changing the currently selected entity

--- code/editor/tools/EntitiesTool.cs
+++ code/editor/tools/EntitiesTool.cs
@@ -16,6 +16,18 @@ namespace Facepunch.CoreWars.Editor
 	public partial class EntitiesTool : EditorTool
 	{
 		[ServerCmd]
+		public static void ChangeLibraryAttributeCmd( string type )
+		{
+			if ( ConsoleSystem.Caller.Pawn is not EditorPlayer player )
+				return;
+
+			if ( player.Tool is not EntitiesTool tool )
+				return;
+
+			tool.SetLibraryAttribute( (EditorEntityLibraryAttribute)Library.GetAttribute( type ) );
+		}
+
+		[ServerCmd]
 		public static void ChangeModeCmd( int mode )
 		{
 			if ( ConsoleSystem.Caller.Pawn is not EditorPlayer player )
@@ -30,14 +42,32 @@ namespace Facepunch.CoreWars.Editor
 		private static EditorEntityLibraryAttribute CurrentEntityAttribute { get; set; }
 
 		[Net, Change( nameof( OnModeChanged ))] public EntitiesToolMode Mode { get; private set; }
+		[Net, Change( nameof( OnLibraryTypeChanged ))] public string CurrentLibraryType { get; private set; }
 
+		private EditorEntityLibraryAttribute CurrentLibraryAttribute { get; set; }
 		private ModelEntity GhostEntity { get; set; }
+
+		public void SetLibraryAttribute( EditorEntityLibraryAttribute attribute )
+		{
+			Host.AssertServer();
+
+			if ( CurrentLibraryType != attribute.Name )
+			{
+				CurrentLibraryType = attribute.Name;
+				CurrentLibraryAttribute = attribute;
+				OnLibraryAttributeChanged( CurrentLibraryAttribute );
+			}
+		}
 
 		public void SetMode( EntitiesToolMode mode )
 		{
 			Host.AssertServer();
-			Mode = mode;
-			OnModeChanged( mode );
+
+			if ( Mode != mode )
+			{
+				Mode = mode;
+				OnModeChanged( mode );
+			}
 		}
 
 		public override void Simulate( Client client )
@@ -82,16 +112,36 @@ namespace Facepunch.CoreWars.Editor
 			}
 		}
 
+		protected virtual void OnLibraryTypeChanged( string type )
+		{
+			CurrentLibraryAttribute = (EditorEntityLibraryAttribute)Library.GetAttribute( type );
+			OnLibraryAttributeChanged( CurrentLibraryAttribute );
+		}
+
+		protected virtual void OnLibraryAttributeChanged( EditorEntityLibraryAttribute attribute )
+		{
+			if ( IsClient )
+			{
+				if ( Mode == EntitiesToolMode.Place )
+				{
+					CreateGhostEntity();
+				}
+			}
+		}
+
 		protected virtual void OnModeChanged( EntitiesToolMode mode )
 		{
-			if ( Mode == EntitiesToolMode.Place )
+			if ( IsClient )
 			{
-				CurrentEntityAttribute = Library.GetAttributes<EditorEntityLibraryAttribute>().FirstOrDefault();
-				CreateGhostEntity();
-			}
-			else
-			{
-				DestroyGhostEntity();
+				if ( Mode == EntitiesToolMode.Place )
+				{
+					CurrentEntityAttribute = Library.GetAttributes<EditorEntityLibraryAttribute>().FirstOrDefault();
+					CreateGhostEntity();
+				}
+				else
+				{
+					DestroyGhostEntity();
+				}
 			}
 		}
 
