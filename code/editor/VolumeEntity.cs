e415bc0d6eba34d4deac0531251b6ee7d49e4d89
kurozael <kurozael@gmail.com>
2022-03-29 21:07:06 +0100

    Added VolumeEntity render entity type to display in editor mode

---
+++ code/editor/VolumeEntity.cs
@@ -0,0 +1,34 @@
+﻿using Facepunch.CoreWars.Utility;
+using Facepunch.Voxels;
+using Sandbox;
+
+namespace Facepunch.CoreWars.Editor
+{
+	public partial class VolumeEntity : RenderEntity
+	{
+		public Material Material = Material.Load( "materials/editor/place_block.vmat" );
+		public Color Color { get; set; }
+
+		public override void DoRender( SceneObject sceneObject )
+		{
+			if ( !EnableDrawing || !VoxelWorld.Current.IsValid() )
+				return;
+
+			var vb = Render.GetDynamicVB( true );
+			var center = RenderBounds.Center;
+			var size = RenderBounds.Size;
+
+			DrawBox( vb, center, size );
+		}
+
+		private void DrawBox( VertexBuffer vb, Vector3 center, Vector3 size )
+		{
+			vb.AddCube( center, size + new Vector3( 0.5f ), Rotation.Identity );
+
+			Render.Attributes.Set( "TintColor", Color );
+			Render.Attributes.Set( "Opacity", 0.8f );
+
+			vb.Draw( Material );
+		}
+	}
+}
