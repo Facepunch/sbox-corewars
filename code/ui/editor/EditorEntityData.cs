8dfb50ab9f492a52e326399c16a3e18322d8a811
kurozael <kurozael@gmail.com>
2022-03-30 16:51:20 +0100

    Simple entity data menu

--- code/ui/editor/EditorEntityData.cs
+++ code/ui/editor/EditorEntityData.cs
@@ -1,18 +1,37 @@
 ﻿using Facepunch.Voxels;
 using Sandbox;
 using Sandbox.UI;
+using Sandbox.UI.Construct;
 using System.Collections.Generic;
+using System.Reflection;
 
 namespace Facepunch.CoreWars.Editor
 {
+	public class EditorEntityDataForm : Form
+	{
+		public EditorEntityDataForm() : base() {
+
+		}
+
+		public void StartGroup()
+		{
+			currentGroup = Add.Panel( "field-group" );
+		}
+
+		public void EndGroup()
+		{
+			currentGroup = null;
+		}
+	}
+
 	[UseTemplate]
 	public partial class EditorEntityData : Panel
 	{
 		public static EditorEntityData Current { get; private set; }
 
+		public EditorEntityDataForm PropertyForm { get; set; }
 		public string Title => Entity.GetType().Name;
 		public ISourceEntity Entity { get; private set; }
-		public Panel Items { get; set; }
 
 		public static void Open( ISourceEntity entity )
 		{
@@ -20,53 +39,113 @@ namespace Facepunch.CoreWars.Editor
 			Current = new EditorEntityData( entity );
 			Current.PopulateItems();
 
-			Game.Hud.AddChild( Current );
+			Game.Hud.FindPopupPanel().AddChild( Current );
 		}
 
 		public EditorEntityData( ISourceEntity entity )
 		{
-			AcceptsFocus = true;
 			Entity = entity;
-			Focus();
 		}
 
 		public void PopulateItems()
 		{
-			Items.DeleteChildren();
+			PropertyForm.Clear();
+			PropertyForm.StartGroup();
 
-			/*
-			var attributes = Library.GetAttributes<EditorEntityLibraryAttribute>();
-
-			foreach ( var attribute in attributes )
+			foreach ( var p in Reflection.GetProperties( Entity ) )
 			{
-				var item = Items.AddChild<EditorEntityItem>();
-				item.SetAttribute( attribute );
-				item.OnSelected = () => OnItemSelected( item );
+				if ( p.GetCustomAttribute<EditorEntityPropertyAttribute>() != null )
+				{
+					PropertyForm.AddRow( p, Entity, CreateControlFor( p ) );
+				}
 			}
-			*/
+
+			PropertyForm.EndGroup();
+
+			var button = PropertyForm.Add.Button( "Save" );
+			button.AddClass( "editor-button" );
+			button.AddEventListener( "onclick", () => Delete() );
 		}
 
-		public override void OnButtonTyped( string button, KeyModifiers km )
+		private Panel CreateControlFor( PropertyInfo property )
 		{
-			if ( button == "escape" )
+			if ( property.PropertyType.IsEnum )
 			{
-				Blur();
-			}
+				var control = new DropDown();
+				var names = property.PropertyType.GetEnumNames();
+				var values = property.PropertyType.GetEnumValues();
 
-			base.OnButtonTyped( button, km );
+				for ( int i = 0; i < names.Length; i++ )
+				{
+					control.Options.Add( new Option( names[i], values.GetValue( i ).ToString() ) );
+				}
+
+				return control;
+			}
+			else if ( property.PropertyType == typeof( float ) )
+			{
+				var range = property.GetCustomAttribute<RangeAttribute>();
+
+				if ( range != null )
+				{
+					var slider = new SliderEntry
+					{
+						MinValue = range.Min,
+						MaxValue = range.Max,
+						Step = range.Step
+					};
+					return slider;
+				}
+
+				var control = new TextEntry
+				{
+					Numeric = true,
+					NumberFormat = "0.###"
+				};
+
+				return control;
+			}
+			else if ( property.PropertyType == typeof( int ) )
+			{
+				var range = property.GetCustomAttribute<RangeAttribute>();
+
+				if ( range != null )
+				{
+					var slider = new SliderEntry
+					{
+						MinValue = range.Min,
+						MaxValue = range.Max,
+						Step = range.Step
+					};
+
+					if ( property.PropertyType == typeof( int ) || property.PropertyType == typeof( uint ) )
+					{
+						slider.TextEntry.NumberFormat = "0.";
+						slider.Slider.Step = 1;
+					}
+
+					return slider;
+				}
+
+				var control = new TextEntry();
+				control.Numeric = true;
+				control.NumberFormat = "0";
+				return control;
+			}
+			else if ( property.PropertyType == typeof( bool ) )
+			{
+				return new Checkbox();
+			}
+			else
+			{
+				return new TextEntry();
+			}
 		}
 
 		protected virtual void OnItemSelected( EditorEntityItem item )
 		{
 			EntitiesTool.ChangeLibraryAttributeCmd( item.Attribute.Name );
 			Delete();
-		}
-
-		protected override void OnBlur( PanelEvent e )
-		{
-			Delete();
-
-			base.OnBlur( e );
 		}
 
 		protected override void PostTemplateApplied()
