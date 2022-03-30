7c3fbb58bb84f1a6e3cf2a89250bbc9a77c9acb4
kurozael <kurozael@gmail.com>
2022-03-30 16:50:12 +0100

    Add popup-container to EditorHud for use with form popups

--- code/ui/editor/EditorHud.cs
+++ code/ui/editor/EditorHud.cs
@@ -7,9 +7,23 @@ namespace Facepunch.CoreWars
 	[UseTemplate]
 	public partial class EditorHud : RootPanel
 	{
+		public Panel PopupPanel { get; set; }
+
 		public EditorHud()
 		{
 			AddChild<ChatBox>();
+		}
+
+		public override Panel FindPopupPanel()
+		{
+			return PopupPanel;
+		}
+
+		protected override void PostTemplateApplied()
+		{
+			PopupPanel.BindClass( "open", () => PopupPanel.ChildrenCount > 0 );
+
+			base.PostTemplateApplied();
 		}
 	}
 }
