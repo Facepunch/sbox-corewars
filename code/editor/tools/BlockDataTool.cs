0b048c604405da7feb100df9ef1b475ad0624af2
kurozael <kurozael@gmail.com>
2022-03-31 11:20:45 +0100

    Added Editor Block Data tool using groundwork done with Editor Entity Data dialog

--- code/editor/tools/BlockDataTool.cs
+++ code/editor/tools/BlockDataTool.cs
@@ -7,6 +7,75 @@ namespace Facepunch.CoreWars.Editor
 	[EditorToolLibrary( Title = "Block Data", Description = "Edit the data of a block", Icon = "textures/ui/tools/blockdata.png" )]
 	public class BlockDataTool : EditorTool
 	{
-		
+		private TimeUntil NextActionTime { get; set; }
+
+		public override void OnSelected()
+		{
+			Event.Register( this );
+
+			NextActionTime = 0.1f;
+		}
+
+		public override void OnDeselected()
+		{
+			Event.Unregister( this );
+		}
+
+		[Event.Tick.Client]
+		protected virtual void ClientTick()
+		{
+			var position = GetTargetBlockPosition();
+			if ( !position.HasValue ) return;
+
+			var blockId = VoxelWorld.Current.GetBlock( position.Value );
+			var block = VoxelWorld.Current.GetBlockType( blockId );
+			var outlineColor = Color.Cyan;
+			var sourceBBox = VoxelWorld.Current.ToSourceBBox( position.Value );
+
+			DebugOverlay.Box(
+				sourceBBox.Mins,
+				sourceBBox.Maxs,
+				outlineColor,
+				Time.Delta,
+				false
+			);
+
+			DebugOverlay.Text( sourceBBox.Center, $"Edit {block.FriendlyName}", Color.Cyan, Time.Delta );
+		}
+
+		protected override void OnPrimary( Client client )
+		{
+			if ( NextActionTime )
+			{
+				var position = GetTargetBlockPosition();
+
+				if ( IsClient && position.HasValue )
+				{
+					EditorBlockData.SendOpenRequest( position.Value.x, position.Value.y, position.Value.z );
+				}
+
+				NextActionTime = 0.1f;
+			}
+		}
+
+		protected override void OnSecondary( Client client )
+		{
+			if ( IsServer )
+			{
+
+			}
+		}
+
+		private IntVector3? GetTargetBlockPosition()
+		{
+			var face = VoxelWorld.Current.Trace( Input.Position * (1.0f / VoxelWorld.Current.VoxelSize), Input.Rotation.Forward, 1000f, out var endPosition, out _ );
+
+			if ( face != BlockFace.Invalid && VoxelWorld.Current.GetBlock( endPosition ) != 0 )
+			{
+				return endPosition;
+			}
+
+			return null;
+		}
 	}
 }
--- code/ui/editor/EditorBlockData.cs
+++ code/ui/editor/EditorBlockData.cs
@@ -11,7 +11,7 @@ using System.Reflection;
 namespace Facepunch.CoreWars.Editor
 {
 	[UseTemplate]
-	public partial class EditorEntityData : Panel
+	public partial class EditorBlockData : Panel
 	{
 		private static object ConvertPropertyValue( PropertyInfo property, string value )
 		{
@@ -32,95 +32,107 @@ namespace Facepunch.CoreWars.Editor
 		}
 
 		[ServerCmd]
-		public static void SaveEntityKeyValue( int entityId, string key, string value )
+		public static void SaveBlockDataValue( int x, int y, int z, string key, string value )
 		{
-			var entity = Sandbox.Entity.FindByIndex( entityId );
-			if ( !entity.IsValid() ) return;
+			var world = VoxelWorld.Current;
+			if ( !world.IsValid() ) return;
 
-			var properties = Reflection.GetProperties( entity );
+			var voxel = world.GetVoxel( x, y, z );
+			var state = world.GetOrCreateState<BlockState>( voxel.Position );
+
+			var properties = Reflection.GetProperties( state );
 
 			foreach ( var property in properties )
 			{
 				if ( property.Name != key ) continue;
-				if ( property.GetCustomAttribute<EditorEntityPropertyAttribute>() == null ) continue;
-
-				property.SetValue( entity, ConvertPropertyValue( property, value ) );
+				if ( property.GetCustomAttribute<PropertyAttribute>() == null ) continue;
+				property.SetValue( state, ConvertPropertyValue( property, value ) );
 			}
+
+			state.IsDirty = true;
 		}
 
-		[ServerCmd( "cw_open_entity_data" )]
-		public static void SendOpenRequest( int entityId )
+		[ServerCmd( "cw_open_block_data" )]
+		public static void SendOpenRequest( int x, int y, int z )
 		{
-			var entity = Sandbox.Entity.FindByIndex( entityId );
-			if ( !entity.IsValid() ) return;
+			var world = VoxelWorld.Current;
+			if ( !world.IsValid() ) return;
+
+			var voxel = world.GetVoxel( x, y, z );
+			var state = world.GetOrCreateState<BlockState>( voxel.Position );
 
 			using ( var stream = new MemoryStream() )
 			{
 				using ( var writer = new BinaryWriter( stream ) )
 				{
-					var properties = Reflection.GetProperties( entity )
-						.Where( property => property.GetCustomAttribute<EditorEntityPropertyAttribute>() != null );
+					var properties = Reflection.GetProperties( state )
+						.Where( property => property.GetCustomAttribute<PropertyAttribute>() != null );
 
 					writer.Write( properties.Count() );
 
 					foreach ( var property in properties )
 					{
-						var value = property.GetValue( entity );
+						var value = property.GetValue( state );
 						writer.Write( property.Name );
 						writer.Write( value.ToString() );
 					}
 				}
 
-				OpenWithValues( To.Single( ConsoleSystem.Caller ), entityId, stream.ToArray() );
+				OpenWithValues( To.Single( ConsoleSystem.Caller ), x, y, z, stream.ToArray() );
 			}
 		}
 
 		[ClientRpc]
-		public static void OpenWithValues( int entityId, byte[] data )
+		public static void OpenWithValues( int x, int y, int z, byte[] data )
 		{
-			var entity = Sandbox.Entity.FindByIndex( entityId );
-			if ( !entity.IsValid() ) return;
+			var world = VoxelWorld.Current;
+			if ( !world.IsValid() ) return;
+
+			var voxel = world.GetVoxel( x, y, z );
+			var state = world.GetOrCreateState<BlockState>( voxel.Position );
 
 			using ( var stream = new MemoryStream( data ) )
 			{
 				using ( var reader = new BinaryReader( stream ) )
 				{
 					var propertyCount = reader.ReadInt32();
-					var properties = Reflection.GetProperties( entity );
+					var properties = Reflection.GetProperties( state );
 
 					for ( var i = 0; i < propertyCount; i++ )
 					{
 						var name = reader.ReadString();
 						var value = reader.ReadString();
 						var property = properties.FirstOrDefault( p => p.Name == name );
-						property.SetValue( entity, ConvertPropertyValue( property, value ) );
+						property.SetValue( state, ConvertPropertyValue( property, value ) );
 					}
 				}
 			}
 
-			Open( (ISourceEntity)entity );
+			Open( voxel, state );
 		}
 
-		public static EditorEntityData Current { get; private set; }
+		public static EditorBlockData Current { get; private set; }
 
 		public SimpleForm PropertyForm { get; set; }
-		public string Title => Entity.GetType().Name;
-		public ISourceEntity Entity { get; private set; }
+		public string Title => Voxel.GetBlockType().FriendlyName;
+		public BlockState State { get; private set; }
+		public Voxel Voxel { get; private set; }
 
 		private Dictionary<string, object> ChangedValues { get; set; } = new();
 
-		public static void Open( ISourceEntity entity )
+		public static void Open( Voxel voxel, BlockState state )
 		{
 			Current?.Delete();
-			Current = new EditorEntityData( entity );
+			Current = new EditorBlockData( voxel, state );
 			Current.PopulateItems();
 
 			Game.Hud.FindPopupPanel().AddChild( Current );
 		}
 
-		public EditorEntityData( ISourceEntity entity )
+		public EditorBlockData( Voxel voxel, BlockState state )
 		{
-			Entity = entity;
+			Voxel = voxel;
+			State = state;
 		}
 
 		public void PopulateItems()
@@ -130,16 +142,16 @@ namespace Facepunch.CoreWars.Editor
 
 			ChangedValues.Clear();
 
-			var properties = Reflection.GetProperties( Entity );
+			var properties = Reflection.GetProperties( State );
 
 			for ( int i = 0; i < properties.Length; i++ )
 			{
 				var property = properties[i];
 
-				if ( property.GetCustomAttribute<EditorEntityPropertyAttribute>() == null )
+				if ( property.GetCustomAttribute<PropertyAttribute>() == null )
 					continue;
 
-				PropertyForm.AddRowWithCallback( property, Entity, PropertyForm.CreateControlFor( property ), ( value ) =>
+				PropertyForm.AddRowWithCallback( property, State, PropertyForm.CreateControlFor( property ), ( value ) =>
 				{
 					ChangedValues[property.Name] = value;
 				} );
@@ -156,7 +168,7 @@ namespace Facepunch.CoreWars.Editor
 		{
 			foreach ( var kv in ChangedValues )
 			{
-				SaveEntityKeyValue( Entity.NetworkIdent, kv.Key, kv.Value.ToString() );
+				SaveBlockDataValue( Voxel.Position.x, Voxel.Position.y, Voxel.Position.z, kv.Key, kv.Value.ToString() );
 			}
 
 			ChangedValues.Clear();
