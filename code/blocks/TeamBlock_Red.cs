e268155df39f5c0bb41274de60dc3c956e3383b3
bakscratch <lt_8@hotmail.co.uk>
2022-04-04 23:28:07 +0100

    Create TeamBlock_Red.cs

---
+++ code/blocks/TeamBlock_Red.cs
@@ -0,0 +1,13 @@
+﻿using Facepunch.Voxels;
+using Sandbox;
+
+namespace Facepunch.CoreWars.Blocks
+{
+	[Library]
+	public class TeamBlock_Red : BlockType
+	{
+		public override string DefaultTexture => "teamcolour_red_01_color";
+		public override string FriendlyName => "Teamcolour Red";
+	}
+}
+
