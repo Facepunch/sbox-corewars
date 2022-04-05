079900716ab11b929259c10e4f0d41b2b20663cf
kurozael <kurozael@gmail.com>
2022-04-05 23:58:26 +0100

    Refactor to NetInventoryContainer and add NetInventoryItem

---
+++ code/inventory/network/NetInventoryContainer.cs
@@ -0,0 +1,38 @@
+﻿using Sandbox;
+
+namespace Facepunch.CoreWars.Inventory
+{
+	public class NetInventoryContainer : BaseNetworkable, INetworkSerializer, IValid
+	{
+		public InventoryContainer Instance { get; private set; }
+		public bool Initialized { get; private set; }
+
+		public bool IsValid => Instance.IsValid();
+
+		public NetInventoryContainer()
+		{
+
+		}
+
+		public NetInventoryContainer( InventoryContainer container )
+		{
+			Instance = container;
+		}
+
+		public void Read( ref NetRead read )
+		{
+			if ( Initialized ) return;
+			var totalBytes = read.Read<int>();
+			var output = new byte[totalBytes];
+			Instance = InventoryContainer.Deserialize( read.ReadUnmanagedArray( output ) );
+			Initialized = true;
+		}
+
+		public void Write( NetWrite write )
+		{
+			var serialized = Instance.Serialize();
+			write.Write( serialized.Length );
+			write.Write( serialized );
+		}
+	}
+}
---
+++ code/inventory/network/NetInventoryItem.cs
@@ -0,0 +1,38 @@
+﻿using Sandbox;
+
+namespace Facepunch.CoreWars.Inventory
+{
+	public class NetInventoryItem : BaseNetworkable, INetworkSerializer, IValid
+	{
+		public InventoryItem Instance { get; private set; }
+		public bool Initialized { get; private set; }
+
+		public bool IsValid => Instance.IsValid();
+
+		public NetInventoryItem()
+		{
+
+		}
+
+		public NetInventoryItem( InventoryItem item )
+		{
+			Instance = item;
+		}
+
+		public void Read( ref NetRead read )
+		{
+			if ( Initialized ) return;
+			var totalBytes = read.Read<int>();
+			var output = new byte[totalBytes];
+			Instance = InventoryItem.Deserialize( read.ReadUnmanagedArray( output ) );
+			Initialized = true;
+		}
+
+		public void Write( NetWrite write )
+		{
+			var serialized = Instance.Serialize();
+			write.Write( serialized.Length );
+			write.Write( serialized );
+		}
+	}
+}
