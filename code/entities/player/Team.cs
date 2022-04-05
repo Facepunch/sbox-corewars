2082c014260a40317382e4dc7660a2f4ff71a0f2
kurozael <kurozael@gmail.com>
2022-04-05 23:00:54 +0100

    Added extensions for Team to get the correct plastic block type and an extension to create a plastic block item

--- code/entities/player/Team.cs
+++ code/entities/player/Team.cs
@@ -1,4 +1,9 @@
-﻿namespace Facepunch.CoreWars
+﻿using Facepunch.CoreWars.Blocks;
+using Facepunch.CoreWars.Inventory;
+using Facepunch.Voxels;
+using Sandbox;
+
+namespace Facepunch.CoreWars
 {
 	public enum Team
 	{
@@ -11,5 +16,40 @@
 		Pink,
 		Cyan,
 		Yellow
+	}
+
+	public static class TeamExtensions
+	{
+		public static BlockType GetPlasticBlock( this Team team )
+		{
+			var world = VoxelWorld.Current;
+			if ( !world.IsValid() ) return null;
+
+			var blockId = team switch
+			{
+				Team.Blue => world.FindBlockId<BluePlasticBlock>(),
+				Team.Red => world.FindBlockId<RedPlasticBlock>(),
+				Team.Orange => world.FindBlockId<OrangePlasticBlock>(),
+				Team.Green => world.FindBlockId<GreenPlasticBlock>(),
+				Team.Purple => world.FindBlockId<PurplePlasticBlock>(),
+				Team.Pink => world.FindBlockId<PinkPlasticBlock>(),
+				Team.Cyan => world.FindBlockId<CyanPlasticBlock>(),
+				Team.Yellow => world.FindBlockId<YellowPlasticBlock>(),
+				_ => throw new System.NotImplementedException()
+			};
+
+			return world.GetBlockType( blockId );
+		}
+
+		public static BlockItem CreatePlasticBlockItem( this Team team )
+		{
+			var block = team.GetPlasticBlock();
+			if ( block == null ) return null;
+
+			var item = InventorySystem.CreateItem<BlockItem>();
+			item.BlockId = block.BlockId;
+
+			return item;
+		}
 	}
 }
