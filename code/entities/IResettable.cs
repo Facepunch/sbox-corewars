703bc02b58cad7deccd0ef667e5611497c2fae7d
kurozael <kurozael@gmail.com>
2022-04-07 00:41:34 +0100

    Add IResettable and IRessetable.ResetAll static member

---
+++ code/entities/IResettable.cs
@@ -0,0 +1,18 @@
+﻿using Sandbox;
+using System.Linq;
+
+namespace Facepunch.CoreWars
+{
+	public interface IResettable
+	{
+		public static void ResetAll()
+		{
+			foreach ( var entity in Entity.All.OfType<IResettable>() )
+			{
+				entity.Reset();
+			}
+		}
+
+		void Reset();
+	}
+}
