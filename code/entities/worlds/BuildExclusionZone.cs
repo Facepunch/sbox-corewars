f397be323dc195aaec9363c1e997bccae10d3867
kurozael <kurozael@gmail.com>
2022-04-06 23:13:25 +0100

    Add BuildExclusionZone

---
+++ code/entities/worlds/BuildExclusionZone.cs
@@ -0,0 +1,28 @@
+﻿using Facepunch.CoreWars.Editor;
+using Sandbox;
+using System.Collections.Generic;
+
+namespace Facepunch.CoreWars
+{
+	[EditorEntity( IsVolume = true, VolumeMaterial = "materials/tools/toolstrigger.vmat" )]
+	public partial class BuildExclusionZone : BaseTrigger
+	{
+		private HashSet<Player> Touching { get; set; } = new();
+
+		public override void StartTouch( Entity other )
+		{
+			if ( other is Player player )
+				Touching.Add( player );
+
+			base.StartTouch( other );
+		}
+
+		public override void EndTouch( Entity other )
+		{
+			if ( other is Player player )
+				Touching.Remove( player );
+
+			base.EndTouch( other );
+		}
+	}
+}
