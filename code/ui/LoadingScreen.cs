08c7bfd356c4b3985472947653bab09a38efbad2
kurozael <kurozael@gmail.com>
2022-04-08 23:16:46 +0100

    Updated the loading screen to use the Core Wars logo and have it spin to indicate it's doing something

--- code/ui/LoadingScreen.cs
+++ code/ui/LoadingScreen.cs
@@ -1,48 +1,24 @@
 ﻿using Facepunch.Voxels;
 using Sandbox;
 using Sandbox.UI;
+using System;
 
 namespace Facepunch.CoreWars
 {
 	[UseTemplate]
 	public partial class LoadingScreen : Panel
 	{
-		public string ChunksLoaded => GetChunksLoaded();
-		
-		private bool DidWorldLoad { get; set; } = false;
+		public Panel Spinner { get; set; }
 
 		public override void Tick()
 		{
 			SetClass( "hidden", HasWorldLoaded() );
 
+			var transform = new PanelTransform();
+			transform.AddRotation( 0f, 0f, MathF.Sin( Time.Now ) * 180f );
+			Spinner.Style.Transform = transform;
+
 			base.Tick();
-		}
-
-		private string GetChunksLoaded()
-		{
-			if ( !VoxelWorld.Current.IsValid() ) return "Generating World...";
-
-			var viewer = Local.Client.GetChunkViewer();
-			if ( !viewer.IsValid() ) return "Initializing Player...";
-
-			if ( !viewer.HasLoadedMinimumChunks() )
-			{
-				var minimumChunks = VoxelWorld.Current.MinimumLoadedChunks;
-				var loadedChunks = viewer.LoadedChunks.Count;
-
-				return $"Loading Minimum Chunks {loadedChunks}/{minimumChunks}";
-			}
-
-			if ( !viewer.IsCurrentChunkReady )
-				return "Loading Spawn Chunk...";
-
-			if ( Local.Client.Pawn is Player player )
-			{
-				if ( player.LifeState == LifeState.Dead )
-					return $"Spawning Player...";
-			}
-
-			return "...";
 		}
 
 		private bool HasWorldLoaded()
@@ -57,6 +33,11 @@ namespace Facepunch.CoreWars
 				return false;
 
 			return viewer.HasLoadedMinimumChunks();
+		}
+
+		protected override void PostTemplateApplied()
+		{
+			base.PostTemplateApplied();
 		}
 	}
 }
