2ac0c6c23ffd54b25c59813b2ca3eaca0257668b
kurozael <kurozael@gmail.com>
2022-04-11 20:29:07 +0100

    Add BaseGenerator for others to derive from with common logic

---
+++ code/entities/worlds/BaseGenerator.cs
@@ -0,0 +1,47 @@
+﻿using Facepunch.CoreWars.Editor;
+using Facepunch.CoreWars.Inventory;
+using Facepunch.Voxels;
+using Sandbox;
+using System;
+using System.IO;
+
+namespace Facepunch.CoreWars
+{
+	public partial class BaseGenerator : ModelEntity, ISourceEntity
+	{
+		private TimeUntil NextGeneration { get; set; }
+
+		public virtual void Serialize( BinaryWriter writer )
+		{
+
+		}
+
+		public virtual void Deserialize( BinaryReader reader )
+		{
+
+		}
+
+		[Event.Tick.Server]
+		protected virtual void ServerTick()
+		{
+			if ( !Game.IsState<GameState>() && !Game.IsState<LobbyState>() )
+				return;
+
+			if ( NextGeneration )
+			{
+				GenerateItems();
+				NextGeneration = CalculateNextGenerationTime();
+			}
+		}
+
+		protected virtual void GenerateItems()
+		{
+
+		}
+
+		protected virtual float CalculateNextGenerationTime()
+		{
+			return 10f;
+		}
+	}
+}
