86c732bd36d9c721d6e1c5a4b0a5a6409bed0bba
kurozael <kurozael@gmail.com>
2022-04-12 16:27:19 +0100

    Remove fgd and use Display attribute

--- code/entities/hammer/VoxelWorldLoader.cs
+++ code/entities/hammer/VoxelWorldLoader.cs
@@ -1,9 +1,11 @@
 ﻿using Sandbox;
+using Hammer;
+using System.ComponentModel.DataAnnotations;
 
 namespace Facepunch.CoreWars
 {
 	[Library( "cw_voxel_world_loader" )]
-	[Hammer.EntityTool( "Voxel World Loader", "Core Wars" )]
+	[Display( Name = "Voxel World Loader", GroupName = "Core Wars" ) ]
 	[Hammer.EditorSprite( "materials/editor/voxel_world_loader.vmat" )]
 	public class VoxelWorldLoader : ModelEntity
 	{
