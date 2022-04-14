ba8d0e5b1d4cd4d4f656f81e031c02d648ab9208
kurozael <kurozael@gmail.com>
2022-04-14 22:27:44 +0100

    Item Store NPC Stub

---
+++ code/entities/worlds/ItemStoreNPC.cs
@@ -0,0 +1,48 @@
+﻿using Facepunch.CoreWars.Editor;
+using Facepunch.Voxels;
+using Sandbox;
+using System;
+using System.IO;
+
+namespace Facepunch.CoreWars
+{
+	[EditorEntity( Title = "Item Store NPC", Group = "Gameplay", EditorModel = "models/citizen/citizen.vmdl" )]
+	public class ItemStoreNPC : AnimEntity, ISourceEntity
+	{
+		[EditorProperty] public Team Team { get; set; }
+
+		public override void Spawn()
+		{
+			SetModel( "models/citizen/citizen.vmdl" );
+			SetupPhysicsFromAABB( PhysicsMotionType.Keyframed, Model.Bounds.Mins, Model.Bounds.Maxs );
+
+			Transmit = TransmitType.Always;
+
+			AddClothing( "models/citizen_clothes/shirt/chainmail/models/chainmail.vmdl" );
+			AddClothing( "models/citizen_clothes/trousers/legarmour/models/leg_armour.vmdl" );
+			AddClothing( "models/citizen_clothes/vest/tactical_vest/models/tactical_vest.vmdl" );
+			AddClothing( "models/citizen_clothes/shoes/trainers/trainers.vmdl" );
+			AddClothing( "models/citizen_clothes/glasses/stylish_glasses/models/stylish_glasses_gold.vmdl" );
+			AddClothing( "models/citizen_clothes/hair/hair_longbrown/models/hair_longbrown.vmdl" );
+
+			base.Spawn();
+		}
+
+		public virtual void Serialize( BinaryWriter writer )
+		{
+			writer.Write( (byte)Team );
+		}
+
+		public virtual void Deserialize( BinaryReader reader )
+		{
+			Team = (Team)reader.ReadByte();
+		}
+
+		public void AddClothing( string modelName )
+		{
+			var clothes = new BaseClothing();
+			clothes.SetModel( modelName );
+			clothes.SetParent( this, true );
+		}
+	}
+}
