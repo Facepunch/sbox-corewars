0693b82ea8c026f63c030cdab53c21c9c22fd01f
kurozael <kurozael@gmail.com>
2022-04-19 23:48:10 +0100

    Added IUsable interface for extended functionality

---
+++ code/entities/IUsable.cs
@@ -0,0 +1,12 @@
+﻿using Sandbox;
+using System.Linq;
+
+namespace Facepunch.CoreWars
+{
+	public interface IUsable
+	{
+		float MaxUseDistance { get; }
+		bool IsUsable( Player player );
+		void OnUsed( Player player );
+	}
+}
