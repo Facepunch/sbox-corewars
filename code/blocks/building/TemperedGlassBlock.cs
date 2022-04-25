be25f1cb4d70bfbb6f45918977d6cde7b340787b
kurozael <kurozael@gmail.com>
2022-04-25 23:32:39 +0100

    Added TemperedGlassBlock and WoodenPlanksBlock

---
+++ code/blocks/building/TemperedGlassBlock.cs
@@ -0,0 +1,13 @@
+﻿using Facepunch.Voxels;
+using Sandbox;
+
+namespace Facepunch.CoreWars.Blocks
+{
+	[Library]
+	public class TemperedGlassBlock : BlockType
+	{
+		public override string DefaultTexture => "tempered_glass_01_color";
+		public override string FriendlyName => "Tempered Glass";
+		public override bool IsTranslucent => true;
+	}
+}
---
+++ code/blocks/building/WoodenPlanksBlock.cs
@@ -0,0 +1,12 @@
+﻿using Facepunch.Voxels;
+using Sandbox;
+
+namespace Facepunch.CoreWars.Blocks
+{
+	[Library]
+	public class WoodenPlanksBlock : BlockType
+	{
+		public override string DefaultTexture => "planks_01_color";
+		public override string FriendlyName => "Wooden Planks";
+	}
+}
