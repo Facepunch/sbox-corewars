964216f04ef0e7e54b25537271e6123e0c88c7ae
kurozael <kurozael@gmail.com>
2022-04-26 00:10:19 +0100

    Added crossbow bolt shop item

---
+++ code/shop/CrossbowBoltShopItem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using Facepunch.CoreWars.Inventory;
+
+namespace Facepunch.CoreWars
+{
+	public class CrossbowBoltShopItem : BasePurchasable
+	{
+		public override string Name => "Crossbow Bolt";
+		public override string Description => "Ammo for Crossbow weapons.";
+		public override int Quantity => 8;
+		public override Dictionary<Type, int> Costs => new()
+		{
+			[typeof( IronItem )] = 16
+		};
+
+		public override string GetIcon( Player player )
+		{
+			return "textures/items/ammo_bolt.png";
+		}
+
+		public override void OnPurchased( Player player )
+		{
+			var item = InventorySystem.CreateItem<AmmoItem>();
+			item.AmmoType = AmmoType.Bolt;
+			item.StackSize = Quantity;
+			player.TryGiveItem( item );
+		}
+	}
+}
