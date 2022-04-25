4aa6ee214e175ae704b095c83cf59acc8c98be5e
kurozael <kurozael@gmail.com>
2022-04-25 23:54:33 +0100

    Added BaseArmorShopItem for handling purchase of armor with dependencies

---
+++ code/shop/armor/BaseArmorShopItem.cs
@@ -0,0 +1,32 @@
+﻿using Facepunch.CoreWars.Inventory;
+using System;
+
+namespace Facepunch.CoreWars
+{
+	public abstract class BaseArmorShopItem<T> : BasePurchasable where T : ArmorItem
+	{
+		public virtual Type PreviousArmorType => null;
+
+		public override bool CanPurchase( Player player )
+		{
+			if ( PreviousArmorType != null )
+			{
+				var items = player.FindItems( PreviousArmorType );
+				return items.Count > 0;
+			}
+
+			return base.CanPurchase( player );
+		}
+
+		public override string GetIcon( Player player )
+		{
+			return string.Empty;
+		}
+
+		public override void OnPurchased( Player player )
+		{
+			var item = InventorySystem.CreateItem<T>();
+			player.TryGiveItem( item );
+		}
+	}
+}
