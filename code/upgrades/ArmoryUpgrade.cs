d6900941592d2f31dba67f1612ac3b0fb7993b3b
kurozael <kurozael@gmail.com>
2022-04-26 01:18:10 +0100

    Added BaseTeamUpgrade and ArmoryUpgrade type and icon

---
+++ code/upgrades/ArmoryUpgrade.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sandbox;
+
+namespace Facepunch.CoreWars
+{
+	[Library]
+	public class ArmoryUpgrade : BaseTeamUpgrade
+	{
+		public override string Name => "Armory";
+		public override string Description => "Unlock new weapons and items to purchase.";
+		public override Dictionary<Type, int> Costs => new()
+		{
+			[typeof( GoldItem )] = 4
+		};
+
+		public override string GetIcon( Player player )
+		{
+			return "textures/upgrades/armory.png";
+		}
+	}
+}
---
+++ code/upgrades/BaseTeamUpgrade.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sandbox;
+
+namespace Facepunch.CoreWars
+{
+	public abstract class BaseTeamUpgrade : BaseNetworkable
+	{
+		public virtual Type PreviousUpgradeType => null;
+		public virtual string Description => string.Empty;
+		public virtual string Name => string.Empty;
+		public virtual Dictionary<Type, int> Costs => new();
+
+		public virtual bool CanAfford( Player player )
+		{
+			foreach ( var kv in Costs )
+			{
+				var sum = player.FindItems( kv.Key ).Sum( i => i.StackSize );
+
+				if ( sum < kv.Value )
+					return false;
+			}
+
+			return true;
+		}
+
+		public virtual string GetIcon( Player player )
+		{
+			return string.Empty;
+		}
+
+		public virtual bool CanPurchase( Player player )
+		{
+			var core = player.Team.GetCore();
+			if ( !core.IsValid() ) return false;
+
+			if ( core.Upgrades.Any( u => u.GetType() == GetType() ) )
+				return false;
+
+			if ( PreviousUpgradeType != null )
+				return core.Upgrades.Any( u => u.GetType() == PreviousUpgradeType );
+
+			return true;
+		}
+
+		public virtual void OnPurchased( Player player )
+		{
+			var core = player.Team.GetCore();
+
+			if ( core.IsValid() )
+			{
+				core.Upgrades.Add( this );
+			}
+		}
+	}
+}
