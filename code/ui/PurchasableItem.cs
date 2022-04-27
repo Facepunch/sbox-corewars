194f2b13b74b2f9f5632abc21545862eba3393b9
kurozael <kurozael@gmail.com>
2022-04-27 23:35:59 +0100

    Add PurchasableItem UI element to be shared between Shop and Upgrades UI

---
+++ code/ui/PurchasableItem.cs
@@ -0,0 +1,85 @@
+﻿using Facepunch.CoreWars.Inventory;
+using Facepunch.Voxels;
+using Sandbox;
+using Sandbox.UI;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using Sandbox.UI.Construct;
+
+namespace Facepunch.CoreWars
+{
+	[UseTemplate]
+	public partial class PurchasableItem : Panel
+	{
+		public Panel Icon { get; set; }
+		public Panel CostContainer { get; set; }
+		public BaseShopItem Item { get; private set; }
+		public Action<BaseShopItem> OnPurchaseClicked { get; set; }
+		public Button PurchaseButton { get; set; }
+
+		public void SetItem( BaseShopItem item )
+		{
+			Item = item;
+
+			CostContainer.DeleteChildren( true );
+
+			if ( Local.Pawn is Player player )
+			{
+				var icon = item.GetIcon( player );
+
+				if ( !string.IsNullOrEmpty( icon ) )
+					Icon.Style.SetBackgroundImage( icon );
+				else
+					Icon.Style.BackgroundImage = null;
+
+				foreach ( var kv in item.Costs )
+				{
+					var itemType = Library.Create<ResourceItem>( kv.Key );
+					if ( itemType == null ) continue;
+
+					var itemCost = kv.Value;
+					var panel = CostContainer.Add.Panel( "cost" );
+
+					panel.Add.Image( itemType.Icon, "icon" );
+					panel.Add.Label( $"x{itemCost}", "value" );
+				}
+			}
+		}
+
+		public void DoPurchase()
+		{
+			Log.Info( "Purchase clicked" );
+
+			OnPurchaseClicked?.Invoke( Item );
+		}
+
+		public bool IsHidden()
+		{
+			if ( Item != null && Local.Pawn is Player player )
+			{
+				return !Item.CanPurchase( player );
+			}
+
+			return true;
+		}
+
+		public bool IsPurchaseDisabled()
+		{
+			if ( Item != null && Local.Pawn is Player player )
+			{
+				return !Item.CanAfford( player );
+			}
+
+			return true;
+		}
+
+		protected override void PostTemplateApplied()
+		{
+			PurchaseButton.BindClass( "disabled", IsPurchaseDisabled );
+			BindClass( "hidden", IsHidden );
+
+			base.PostTemplateApplied();
+		}
+	}
+}
