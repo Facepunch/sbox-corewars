9e38a77e840ec0d05b1d443af67131eca4ef65fe
kurozael <kurozael@gmail.com>
2022-05-04 14:26:16 +0100

    Set BuildingBlockState health to 100 on creation

---
+++ code/blocks/building/BaseBuildingBlock.cs
@@ -0,0 +1,19 @@
+﻿using Facepunch.Voxels;
+using Sandbox;
+
+namespace Facepunch.CoreWars.Blocks
+{
+	public class BaseBuildingBlock : BlockType
+	{
+		public virtual BuildingMaterialType MaterialType => BuildingMaterialType.Unbreakable;
+		public virtual float DamageMultiplier => 1f;
+
+		public override BlockState CreateState()
+		{
+			var state = new BuildingBlockState();
+			state.Health = 100;
+			return state;
+		}
+	}
+}
+
