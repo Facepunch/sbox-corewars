7ac02f764a2cb06b4e6e705ba4d220cff61089ea
kurozael <kurozael@gmail.com>
2022-05-05 13:34:23 +0100

    Remove portal grenades on death

--- code/items/weapons/PortalGrenadeItem.cs
+++ code/items/weapons/PortalGrenadeItem.cs
@@ -7,6 +7,7 @@ namespace Facepunch.CoreWars
 	[Library( "item_portal_grenade" )]
 	public class PortalGrenadeItem : WeaponItem
 	{
+		public override bool RemoveOnDeath => true;
 		public override string WeaponName => "weapon_portal_grenade";
 		public override string Icon => "textures/items/portal_grenade.png";
 		public override string Name => "Portal Grenade";
