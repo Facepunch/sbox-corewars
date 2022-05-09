b6a914cf3c6c3eecdf415c8e35a86251e284305e
kurozael <kurozael@gmail.com>
2022-05-09 12:20:32 +0100

    Added ToastList and Toast methods to Hud

--- code/ui/Hud.cs
+++ code/ui/Hud.cs
@@ -8,9 +8,44 @@ namespace Facepunch.CoreWars
 	[UseTemplate]
 	public partial class Hud : RootPanel
 	{
+		[ServerCmd]
+		public static void TestToastCmd( string text )
+		{
+			Toast( To.Everyone, text, "textures/items/crystal.png" );
+		}
+
+		[ClientRpc]
+		public static void AddKillFeed( Player attacker, Player victim, Entity weapon )
+		{
+			ToastList.Instance.AddKillFeed( attacker, victim, weapon );
+		}
+
+		[ClientRpc]
+		public static void AddKillFeed( Player victim )
+		{
+			ToastList.Instance.AddKillFeed( victim );
+		}
+
+		public static void ToastAll( string text, string icon = "" )
+		{
+			Toast( To.Everyone, text, icon );
+		}
+
+		public static void Toast( Player player, string text, string icon = "" )
+		{
+			Toast( To.Single( player ), text, icon );
+		}
+
+		[ClientRpc]
+		public static void Toast( string text, string icon = "" )
+		{
+			ToastList.Instance.AddItem( text, Texture.Load( FileSystem.Mounted, icon ) );
+		}
+
 		public Hud()
 		{
 			AddChild<ChatBox>();
+			AddChild<ToastList>();
 			AddChild<VoiceList>();
 		}
 	}
