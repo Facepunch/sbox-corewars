416df9e6131b6d58cc148f7e0acfc4112ce9e6a9
kurozael <kurozael@gmail.com>
2022-05-10 16:02:23 +0100

    Add Mins and Maxs to IVolumeEntity

--- code/editor/IVolumeEntity.cs
+++ code/editor/IVolumeEntity.cs
@@ -3,10 +3,12 @@ using Sandbox;
 
 namespace Facepunch.CoreWars.Editor
 {
-	public interface IVolumeEntity
+	public interface IVolumeEntity : IValid
 	{
 		Vector3 Position { get; set; }
 		Rotation Rotation { get; set; }
+		Vector3 Mins { get; set; }
+		Vector3 Maxs { get; set; }
 		void SetVolume( Vector3 mins, Vector3 maxs );
 	}
 }
