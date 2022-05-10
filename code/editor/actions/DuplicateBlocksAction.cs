e0a0317a66bb3b140653e75f4c085ab983c24916
kurozael <kurozael@gmail.com>
2022-05-10 16:02:58 +0100

    When holding shift and using the Duplicate Blocks tool, entities will also be copy and pasted

--- code/editor/actions/DuplicateBlocksAction.cs
+++ code/editor/actions/DuplicateBlocksAction.cs
@@ -2,6 +2,8 @@
 using Sandbox;
 using System.Collections.Generic;
 using System.Linq;
+using System.IO;
+using System;
 
 namespace Facepunch.CoreWars.Editor
 {
@@ -18,8 +20,10 @@ namespace Facepunch.CoreWars.Editor
 		private IntVector3 SourceMins { get; set; }
 		private IntVector3 SourceMaxs { get; set; }
 		private IntVector3 TargetMins { get; set; }
+		private List<int> EntityIds { get; set; }
+		private bool CopyEntities { get; set; }
 
-		public void Initialize( IntVector3 sourceMins, IntVector3 sourceMaxs, IntVector3 targetMins )
+		public void Initialize( IntVector3 sourceMins, IntVector3 sourceMaxs, IntVector3 targetMins, bool copyEntities = false )
 		{
 			var world = VoxelWorld.Current;
 
@@ -36,6 +40,8 @@ namespace Facepunch.CoreWars.Editor
 			OldBlockIds = new byte[totalBlocks];
 			NewBlockStates = new BlockState[totalBlocks];
 			NewBlockIds = new byte[totalBlocks];
+			CopyEntities = copyEntities;
+			EntityIds = new();
 
 			foreach ( var position in SourcePositions )
 			{
@@ -70,6 +76,11 @@ namespace Facepunch.CoreWars.Editor
 				world.SetState( TargetPositions[i], NewBlockStates[i] );
 			}
 
+			if ( CopyEntities )
+			{
+				PasteEntities();
+			}
+
 			base.Perform();
 		}
 
@@ -83,7 +94,74 @@ namespace Facepunch.CoreWars.Editor
 				world.SetState( TargetPositions[i], OldBlockStates[i] );
 			}
 
+			foreach ( var entityId in EntityIds )
+			{
+				if ( FindObject<Entity>( entityId, out var entity ) )
+				{
+					entity.Delete();
+				}
+			}
+
 			base.Undo();
+		}
+
+		private void PasteEntities()
+		{
+			EntityIds.Clear();
+
+			var world = VoxelWorld.Current;
+			var sourceMins = world.ToSourcePosition( SourceMins );
+			var sourceMaxs = world.ToSourcePosition( SourceMaxs );
+			var targetMins = world.ToSourcePosition( TargetMins );
+			var minX = Math.Min( sourceMins.x, sourceMaxs.x );
+			var minY = Math.Min( sourceMins.y, sourceMaxs.y );
+			var minZ = Math.Min( sourceMins.z, sourceMaxs.z );
+			var maxX = Math.Max( sourceMins.x, sourceMaxs.x );
+			var maxY = Math.Max( sourceMins.y, sourceMaxs.y );
+			var maxZ = Math.Max( sourceMins.z, sourceMaxs.z );
+			var entities = Entity.FindInBox( new BBox( new Vector3( minX, minY, minZ ), new Vector3( maxX, maxY, maxZ ) ) );
+
+			foreach ( var entity in entities )
+			{
+				if ( entity is ISourceEntity sourceEntity )
+				{
+					var localPosition = entity.Position - sourceMins;
+					var newPosition = targetMins + localPosition;
+					var newEntity = Library.Create<ISourceEntity>( entity.GetType() );
+
+					newEntity.Position = newPosition;
+					newEntity.Rotation = entity.Rotation;
+
+					IVolumeEntity volumeEntity = default;
+					Vector3 oldMins = default;
+					Vector3 oldMaxs = default;
+
+					if ( entity is IVolumeEntity )
+					{
+						volumeEntity = (entity as IVolumeEntity);
+
+						var localMins = volumeEntity.Mins - sourceMins;
+						var localMaxs = volumeEntity.Maxs - sourceMins;
+
+						oldMins = volumeEntity.Mins;
+						oldMaxs = volumeEntity.Maxs;
+
+						volumeEntity.Mins = targetMins + localMins;
+						volumeEntity.Maxs = targetMins + localMaxs;
+					}
+
+					var serialized = BinaryHelper.Serialize( w => sourceEntity.Serialize( w ) );
+					BinaryHelper.Deserialize( serialized, r => newEntity.Deserialize( r ) );
+
+					if ( volumeEntity.IsValid() )
+					{
+						volumeEntity.Mins = oldMins;
+						volumeEntity.Maxs = oldMaxs;
+					}
+
+					EntityIds.Add( AddObject( newEntity ) );
+				}
+			}
 		}
 	}
 }
