4ff2966d9e875def41280fd64d885fb5d109e769
kurozael <kurozael@gmail.com>
2022-05-12 13:33:44 +0100

    Add Announcement and Announcements UI items

---
+++ code/ui/Announcement.cs
@@ -0,0 +1,40 @@
+﻿using Sandbox.UI;
+using Sandbox;
+using System;
+
+namespace Facepunch.CoreWars
+{
+	[UseTemplate]
+	public class Announcement : Panel
+	{
+		public float EndTime { get; set; }
+		public Label Reward { get; set; }
+		public Label Title { get; set; }
+		public Label Text { get; set; }
+		public Image Icon { get; set; }
+
+		public void Update( string title, string text )
+		{
+			Title.Text = title;
+			Text.Text = text;
+		}
+
+		public void SetIcon( string icon )
+		{
+			Icon.SetTexture( icon );
+		}
+
+		public void SetIcon( Texture texture )
+		{
+			Icon.Texture = texture;
+		}
+
+		public override void Tick()
+		{
+			if ( !IsDeleting && Time.Now >= EndTime )
+			{
+				Delete();
+			}
+		}
+	}
+}
---
+++ code/ui/Announcements.cs
@@ -0,0 +1,58 @@
+﻿using Sandbox;
+using Sandbox.UI;
+using Sandbox.UI.Construct;
+using System;
+using System.Collections.Generic;
+
+namespace Facepunch.CoreWars
+{
+	public partial class Announcements : Panel
+	{
+		public static Announcements Instance { get; private set; }
+		public Queue<Announcement> Queue { get; private set; }
+		public Panel Container { get; set; }
+
+		[ClientRpc]
+		public static void Send( string title, string text, string icon )
+		{
+			var announcement = new Announcement();
+			announcement.Update( title, text );
+			announcement.SetIcon( icon );
+			Instance.AddItem( announcement );
+		}
+
+		public Announcements()
+		{
+			StyleSheet.Load( "/ui/Announcements.scss" );
+			Container = Add.Panel( "container" );
+			Instance = this;
+			Queue = new();
+		}
+
+		public void AddItem( Announcement item )
+		{
+			Queue.Enqueue( item );
+		}
+
+		public void Next()
+		{
+			if ( Queue.Count > 0 )
+			{
+				Audio.Play( "ui.announcement" );
+				var item = Queue.Dequeue();
+				item.EndTime = Time.Now + 5f;
+				Container.AddChild( item );
+			}
+		}
+
+		public override void Tick()
+		{
+			if ( Queue.Count > 0 && Container.ChildrenCount == 0 )
+			{
+				Next();
+			}
+
+			base.Tick();
+		}
+	}
+}
