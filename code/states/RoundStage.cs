8b0be0c06e8fa0cc0aef727025b13e8f666952f1
kurozael <kurozael@gmail.com>
2022-05-12 12:32:34 +0100

    Add RoundStage enum

---
+++ code/states/RoundStage.cs
@@ -0,0 +1,19 @@
+﻿using Sandbox;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Facepunch.CoreWars
+{
+	public enum RoundStage
+	{
+		None,
+		Start,
+		GoldII,
+		CrystalII,
+		GoldIII,
+		CrystalIII,
+		NoBeds,
+		SuddenDeath,
+		End
+	}
+}
