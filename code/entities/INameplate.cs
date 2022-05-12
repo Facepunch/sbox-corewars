acfd00b2411e9cb38fc51103fffd60fff9f02e2d
kurozael <kurozael@gmail.com>
2022-05-12 11:52:06 +0100

    Add INameplate interface for entities to implement

---
+++ code/entities/INameplate.cs
@@ -0,0 +1,15 @@
+﻿using Sandbox;
+using System.Linq;
+
+namespace Facepunch.CoreWars
+{
+	public interface INameplate : IValid
+	{
+		public BBox WorldSpaceBounds { get; }
+		public string DisplayName { get; }
+		public bool IsFriendly { get; }
+		public LifeState LifeState { get; }
+		public Vector3 Position { get; }
+		public Team Team { get; }
+	}
+}
