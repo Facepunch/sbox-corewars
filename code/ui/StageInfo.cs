d610d983097a8c5d79ef8fb6adf8212ac202c17b
kurozael <kurozael@gmail.com>
2022-05-18 18:06:08 +0100

    Add StageInfo to show Stage, Time Left and Next Stage

---
+++ code/ui/StageInfo.cs
@@ -0,0 +1,62 @@
+﻿using Facepunch.CoreWars.Inventory;
+using Facepunch.Voxels;
+using Sandbox;
+using Sandbox.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Facepunch.CoreWars
+{
+	[UseTemplate]
+	public partial class StageInfo : Panel
+	{
+		public string NextStageText => GetNextStageText();
+		public string StageText => GetStageText();
+		public string TimeText => GetTimeText();
+
+		protected override void PostTemplateApplied()
+		{
+			BindClass( "hidden", IsHidden );
+			base.PostTemplateApplied();
+		}
+
+		private string GetNextStageText()
+		{
+			if ( Game.TryGetState<GameState>( out var state ) )
+			{
+				return state.Stage.GetNextStage().GetName();
+			}
+
+			return string.Empty;
+		}
+
+		private string GetStageText()
+		{
+			if ( Game.TryGetState<GameState>( out var state ) )
+			{
+				return state.Stage.GetName();
+			}
+
+			return string.Empty;
+		}
+
+		private string GetTimeText()
+		{
+			if ( Game.TryGetState<GameState>( out var state ) )
+			{
+				return TimeSpan.FromSeconds( state.NextStageTime.Absolute ).ToString( @"mm\:ss" );
+			}
+
+			return string.Empty;
+		}
+
+		private bool IsHidden()
+		{
+			if ( IDialog.IsActive() || !Game.IsState<GameState>() )
+				return true;
+
+			return false;
+		}
+	}
+}
