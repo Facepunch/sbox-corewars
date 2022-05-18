df4d967e4eb035b64d443c1272fb60a9ca405b54
kurozael <kurozael@gmail.com>
2022-05-18 18:05:58 +0100

    Add RoundStageExtensions to get the next stage and a stage name

--- code/states/RoundStage.cs
+++ code/states/RoundStage.cs
@@ -16,4 +16,40 @@ namespace Facepunch.CoreWars
 		SuddenDeath,
 		End
 	}
+
+	public static class RoundStageExtensions
+	{
+		public static RoundStage GetNextStage( this RoundStage stage )
+		{
+			if ( stage + 1 > RoundStage.End )
+				return RoundStage.None;
+			else
+				return stage + 1;
+		}
+
+		public static string GetName( this RoundStage stage )
+		{
+			switch ( stage )
+			{
+				case RoundStage.Start:
+					return "Start";
+				case RoundStage.GoldII:
+					return "Gold II";
+				case RoundStage.CrystalII:
+					return "Crystal II";
+				case RoundStage.GoldIII:
+					return "Gold III";
+				case RoundStage.CrystalIII:
+					return "Crystal III";
+				case RoundStage.NoBeds:
+					return "No Beds";
+				case RoundStage.SuddenDeath:
+					return "Sudden Death";
+				case RoundStage.End:
+					return "Game Over";
+			}
+
+			return string.Empty;
+		}
+	}
 }
