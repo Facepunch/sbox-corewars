9b70b08c204a7bd24c9f00db60bdcf6113cb8b4c
bakscratch <lt_8@hotmail.co.uk>
2022-05-25 12:30:32 +0100

    World loader as HammerEntity

--- code/entities/hammer/VoxelWorldLoader.cs
+++ code/entities/hammer/VoxelWorldLoader.cs
@@ -7,6 +7,7 @@ namespace Facepunch.CoreWars
 	[Library( "cw_voxel_world_loader" )]
 	[Title( "Voxel World Loader" ) ]
 	[EditorSprite( "materials/editor/voxel_world_loader.vmat" )]
+	[HammerEntity]
 	public class VoxelWorldLoader : ModelEntity
 	{
 		[Property] public string FileName { get; set; }
