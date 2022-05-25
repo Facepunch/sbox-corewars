d974bbfb5ff182aa27a8aad6b6a05ec53ed14e03
kurozael <kurozael@gmail.com>
2022-05-25 17:30:31 +0100

    Add ModelName to ArmorItem

--- code/items/ArmorItem.cs
+++ code/items/ArmorItem.cs
@@ -10,6 +10,7 @@ namespace Facepunch.CoreWars
 
 		public virtual float DamageMultiplier => 1f;
 		public virtual ArmorSlot ArmorSlot => ArmorSlot.None;
+		public virtual string ModelName => string.Empty;
 		public virtual int Tier => 0;
 
 		public override bool CanStackWith( InventoryItem other )
