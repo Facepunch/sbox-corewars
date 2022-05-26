aaedc06a0a9cab6b80540759c074d42966344132
kurozael <kurozael@gmail.com>
2022-05-26 14:21:06 +0100

    Add MirrorBlocksTool (no mirror logic yet)

---
+++ code/editor/tools/MirrorBlocksTool.cs
@@ -0,0 +1,188 @@
+﻿using Facepunch.CoreWars.Blocks;
+using Facepunch.Voxels;
+using Sandbox;
+
+namespace Facepunch.CoreWars.Editor
+{
+	[EditorTool( Title = "Mirror Blocks", Description = "Mirror an area of blocks" )]
+	[Icon( "textures/ui/tools/duplicateblocks.png" )]
+	public partial class MirrorBlocksTool : EditorTool
+	{
+		public enum MirrorStage
+		{
+			Select,
+			Mirror
+		}
+
+		[Net] public MirrorStage Stage { get; set; } = MirrorStage.Select;
+
+		private EditorAreaGhost AreaGhost { get; set; }
+		private TimeUntil NextBlockPlace { get; set; }
+		private Vector3? StartPosition { get; set; }
+		private Vector3? EndPosition { get; set; }
+
+		[ConCmd.Server( "mirror_blocks_cancel" )]
+		public static void SendCancelCmd()
+		{
+			if ( ConsoleSystem.Caller.Pawn is EditorPlayer player )
+			{
+				if ( player.Tool is MirrorBlocksTool tool )
+				{
+					tool.Cancel();
+				}
+			}
+		}
+
+		[ConCmd.Server( "mirror_blocks_mirror" )]
+		public static void SendMirrorCmd()
+		{
+			if ( ConsoleSystem.Caller.Pawn is EditorPlayer player )
+			{
+				if ( player.Tool is MirrorBlocksTool tool )
+				{
+					tool.Mirror();
+				}
+			}
+		}
+
+		public void Cancel()
+		{
+			if ( IsClient )
+			{
+				SendCancelCmd();
+			}
+
+			NextBlockPlace = 0.1f;
+			StartPosition = null;
+			EndPosition = null;
+			Stage = MirrorStage.Select;
+		}
+
+		public void Mirror()
+		{
+			if ( IsClient )
+			{
+				SendMirrorCmd();
+			}
+
+			if ( Stage == MirrorStage.Mirror )
+			{
+
+			}
+		}
+
+		public override void Simulate( Client client )
+		{
+			var currentMap = VoxelWorld.Current;
+
+			if ( IsClient && currentMap.IsValid() && AreaGhost.IsValid() )
+			{
+				var aimVoxelPosition = GetAimVoxelPosition( 6f );
+				var aimSourcePosition = VoxelWorld.Current.ToSourcePosition( aimVoxelPosition );
+
+				if ( Stage == MirrorStage.Select )
+				{
+					if ( StartPosition.HasValue )
+					{
+						AreaGhost.StartBlock = new BBox( StartPosition.Value, StartPosition.Value + new Vector3( VoxelWorld.Current.VoxelSize ) );
+						AreaGhost.EndBlock = new BBox( aimSourcePosition, aimSourcePosition + new Vector3( VoxelWorld.Current.VoxelSize ) );
+						AreaGhost.UpdateRenderBounds();
+					}
+					else
+					{
+						AreaGhost.StartBlock = new BBox( aimSourcePosition, aimSourcePosition + new Vector3( VoxelWorld.Current.VoxelSize ) );
+						AreaGhost.EndBlock = AreaGhost.StartBlock;
+						AreaGhost.UpdateRenderBounds();
+					}
+
+					AreaGhost.Color = Color.Orange;
+				}
+			}
+
+			base.Simulate( client );
+		}
+
+		public override void OnSelected()
+		{
+			if ( IsClient )
+			{
+				VoxelWorld.Current.GlobalOpacity = 0.8f;
+
+				AreaGhost = new EditorAreaGhost
+				{
+					RenderBounds = new BBox( Vector3.One * -100f, Vector3.One * 100f ),
+					EnableDrawing = true,
+					Color = Color.Orange
+				};
+
+				Player.Camera.ZoomOut = 1f;
+			}
+
+			StartPosition = null;
+			EndPosition = null;
+			Stage = MirrorStage.Select;
+		}
+
+		public override void OnDeselected()
+		{
+			if ( IsClient )
+			{
+				Player.Camera.ZoomOut = 0f;
+				VoxelWorld.Current.GlobalOpacity = 1f;
+				AreaGhost?.Delete();
+			}
+		}
+
+		protected override void OnPrimary( Client client )
+		{
+			if ( NextBlockPlace )
+			{
+				var aimVoxelPosition = GetAimVoxelPosition( 6f );
+				var aimSourcePosition = VoxelWorld.Current.ToSourcePosition( aimVoxelPosition );
+
+				if ( Stage == MirrorStage.Select )
+				{
+					if ( StartPosition.HasValue )
+					{
+						if ( IsClient )
+						{
+							EditorMirrorMenu.Open();
+						}
+
+						EndPosition = aimSourcePosition;
+						Stage = MirrorStage.Mirror;
+					}
+					else
+					{
+						StartPosition = aimSourcePosition;
+					}
+				}
+				else
+				{
+					if ( IsServer )
+					{
+						/*
+						var startSourceVoxelPosition = VoxelWorld.Current.ToVoxelPosition( StartPosition.Value );
+						var endSourceVoxelPosition = VoxelWorld.Current.ToVoxelPosition( EndPosition.Value );
+
+						var action = new DuplicateBlocksAction();
+						action.Initialize( startSourceVoxelPosition, endSourceVoxelPosition, aimVoxelPosition, Input.Down( InputButton.Run ) );
+
+						Player.Perform( action );
+						*/
+					}
+				}
+
+				NextBlockPlace = 0.1f;
+			}
+		}
+
+		protected override void OnSecondary( Client client )
+		{
+			if ( NextBlockPlace )
+			{
+				Cancel();
+			}
+		}
+	}
+}
