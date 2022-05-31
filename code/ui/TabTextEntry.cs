da03eff71aed419577716b4f37fedd73f34e6d0b
kurozael <kurozael@gmail.com>
2022-05-31 11:45:32 +0100

    Import ChatBox from Hover for Lewis to style

---
+++ code/ui/TabTextEntry.cs
@@ -0,0 +1,43 @@
+﻿
+using Sandbox;
+using Sandbox.Hooks;
+using Sandbox.UI;
+using Sandbox.UI.Construct;
+using System;
+
+namespace Facepunch.CoreWars
+{
+	public partial class TabTextEntry : TextEntry
+	{
+		public event Action OnTabPressed;
+		
+		public bool IsShiftDown { get; private set; }
+
+		public override void OnButtonTyped( string button, KeyModifiers km )
+		{
+			if ( button == "tab" )
+			{
+				OnTabPressed?.Invoke();
+				return;
+			}
+
+			base.OnButtonTyped( button, km );
+		}
+
+		public override void OnButtonEvent( ButtonEvent e )
+		{
+			if ( e.Button == "lshift" )
+			{
+				IsShiftDown = e.Pressed;
+			}
+
+			base.OnButtonEvent( e );
+		}
+
+		protected override void OnBlur( PanelEvent e )
+		{
+			IsShiftDown = false;
+			base.OnBlur( e );
+		}
+	}
+}
