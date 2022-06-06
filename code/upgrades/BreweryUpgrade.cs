9652c2a8bad46dc94403533878d8358c6167730d
kurozael <kurozael@gmail.com>
2022-06-06 15:56:08 +0100

    Add BreweryUpgrade and icons for each brew

---
+++ code/upgrades/BreweryUpgrade.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sandbox;
+
+namespace Facepunch.CoreWars
+{
+	[Library]
+	public class BreweryUpgrade : BaseTeamUpgrade
+	{
+		public override string Name => "Brewery";
+		public override string Description => "Unlock special consumable brews.";
+		public override Dictionary<Type, int> Costs => new()
+		{
+			[typeof( GoldItem )] = 4
+		};
+
+		public override string GetIcon( Player player )
+		{
+			return "textures/upgrades/brewery.png";
+		}
+	}
+}
