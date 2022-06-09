289a78d85a5b8b992cdf59f8bc530ec0ce9dadcd
kurozael <kurozael@gmail.com>
2022-06-09 13:10:23 +0100

    Updated description of VortexBombShopItem

--- code/shop/blocks/VortexBombShopItem.cs
+++ code/shop/blocks/VortexBombShopItem.cs
@@ -9,7 +9,7 @@ namespace Facepunch.CoreWars
 	[Library]
 	public class VortexBombShopItem : BaseBlockShopItem
 	{
-		public override string Description => "A high strength timed explosives block.";
+		public override string Description => "A high damage timed explosive bomb which can only be neutralized with a Watergun.";
 		public override string Name => "Vortex Bomb";
 		public override Dictionary<Type, int> Costs => new()
 		{
