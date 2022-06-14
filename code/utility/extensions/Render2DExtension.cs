d40d2d48dc13a0eb8768660d3127a22c4a645d9b
kurozael <kurozael@gmail.com>
2022-06-14 13:27:37 +0100

    Added Render2DExtension and Render2D.Image

---
+++ code/utility/extensions/Render2DExtension.cs
@@ -0,0 +1,17 @@
+﻿using Sandbox;
+
+namespace Facepunch.CoreWars.Utility
+{
+	public static class Render2DExtension
+	{
+		/// <summary>
+		/// Draw an image to the screen.
+		/// </summary>
+		public static void Image( this Sandbox.Render.Render2D self, string texture, Rect rect )
+		{
+			self.Material = null;
+			self.Texture = Texture.Load( FileSystem.Mounted, texture );
+			self.Quad( rect.TopLeft, rect.TopRight, rect.BottomRight, rect.BottomLeft );
+		}
+	}
+}
