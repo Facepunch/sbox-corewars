3f8fd117543731058a3416b563650201ed61ff7a
kurozael <kurozael@gmail.com>
2022-06-15 15:02:08 +0100

    Add test detail models to DevBlock

--- code/blocks/DevBlock.cs
+++ code/blocks/DevBlock.cs
@@ -8,5 +8,17 @@ namespace Facepunch.CoreWars.Blocks
 	{
 		public override string DefaultTexture => "dev_grid";
 		public override string FriendlyName => "DevBlock";
+		public override float DetailSpawnChance => 0.05f;
+		public override string[] DetailModels => new string[]
+		{
+			"models/rust_nature/overgrowth/patch_grass_small.vmdl",
+			"models/rust_nature/overgrowth/patch_grass_tall.vmdl",
+			"models/rust_nature/overgrowth/cracks_grass_b.vmdl"
+		};
+
+		protected override void OnSpawnDetailModel( ModelEntity entity )
+		{
+			entity.Scale = 0.5f;
+		}
 	}
 }
