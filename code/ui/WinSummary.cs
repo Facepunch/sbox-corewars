708a078e769942e165d01989d1bbf4675a830bc3
kurozael <kurozael@gmail.com>
2022-07-15 23:52:50 +0100

    Finish rest of HTML template + add references for lists

--- code/ui/WinSummary.cs
+++ code/ui/WinSummary.cs
@@ -7,6 +7,9 @@ namespace Facepunch.CoreWars
 	{
 		public static WinSummary Current { get; private set; }
 
+		public Panel CoresDestroyedList { get; private set; }
+		public Panel MostKillsList { get; private set; }
+		public string NextRoundTime => "0";
 		public bool IsOpen { get; set; }
 
 		public WinSummary()
@@ -27,6 +30,12 @@ namespace Facepunch.CoreWars
 			if ( !IsOpen ) return;
 			IDialog.Deactivate( this );
 			IsOpen = false;
+		}
+
+		public void Populate()
+		{
+			CoresDestroyedList.DeleteChildren( true );
+			MostKillsList.DeleteChildren( true );
 		}
 
 		protected override void PostTemplateApplied()
