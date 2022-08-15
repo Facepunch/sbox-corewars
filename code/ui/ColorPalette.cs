eedaafffb5e15e592a13ee96f77d04a818c01524
kurozael <kurozael@gmail.com>
2022-08-15 16:41:33 +0100

    Add TeamCoreList to show which teams still have their cores remaining in the bottom right

--- code/ui/ColorPalette.cs
+++ code/ui/ColorPalette.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Facepunch.CoreWars
+﻿namespace Facepunch.CoreWars
 {
 	public static class ColorPalette
 	{
---
+++ code/ui/TeamCoreList.cs
@@ -0,0 +1,63 @@
+﻿using Facepunch.CoreWars.Inventory;
+using Sandbox;
+using Sandbox.UI;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Facepunch.CoreWars
+{
+	[UseTemplate]
+	public partial class TeamCoreList : Panel
+	{
+		public static TeamCoreList Current { get; private set; }
+
+		private class TeamCoreIcon : Panel
+		{
+			private TeamCore Core { get; set; }
+
+			public TeamCoreIcon()
+			{
+				AddClass( "core" );
+			}
+
+			public void SetCoreEntity( TeamCore core )
+			{
+				Style.BackgroundTint = core.Team.GetColor();
+				Core = core;
+			}
+
+			public override void Tick()
+			{
+				SetClass( "destroyed", !Core.IsValid() || Core.LifeState == LifeState.Dead );
+				base.Tick();
+			}
+		}
+
+		private Panel Container { get; set; }
+
+		protected override void PostTemplateApplied()
+		{
+			BindClass( "hidden", IsHidden );
+
+			foreach ( var core in Entity.All.OfType<TeamCore>() )
+			{
+				var icon = new TeamCoreIcon();
+				icon.SetCoreEntity( core );
+				Container.AddChild( icon );
+			}
+
+			base.PostTemplateApplied();
+		}
+
+		private bool IsHidden()
+		{
+			if ( Local.Pawn.LifeState == LifeState.Dead )
+				return true;
+
+			if ( IDialog.IsActive() || !Game.IsState<GameState>() )
+				return true;
+
+			return false;
+		}
+	}
+}
