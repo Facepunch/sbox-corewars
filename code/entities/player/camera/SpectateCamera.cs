e8a596e1f0eb4ea958f29260c2cc808322f5dc35
kurozael <kurozael@gmail.com>
2022-08-16 14:07:02 +0100

    Add SpectateCamera

---
+++ code/entities/player/camera/SpectateCamera.cs
@@ -0,0 +1,34 @@
+﻿using Sandbox;
+
+namespace Facepunch.CoreWars
+{
+	public partial class SpectateCamera : CameraMode
+	{
+		public override void Activated()
+		{
+			base.Activated();
+
+			if ( Local.Pawn is Player player )
+			{
+				Position = player.EyePosition;
+				Rotation = player.EyeRotation;
+			}
+
+			FieldOfView = 80f;
+		}
+
+		public override void Update()
+		{
+			if ( Local.Pawn is not Player player )
+				return;
+
+			Position = player.EyePosition;
+
+			if ( player.Ragdoll.IsValid() )
+			{
+				var direction = (player.Ragdoll.PhysicsBody.Position - Position).Normal;
+				Rotation = Rotation.Slerp( Rotation, Rotation.LookAt( direction ), Time.Delta * 4f );
+			}
+		}
+	}
+}
