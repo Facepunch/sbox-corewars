2089b210b0980d2ebe74f3517cf62cedd074fdd0
kurozael <kurozael@gmail.com>
2022-09-20 23:49:02 +0100

    Adjust text shadow for tooltip - trying to get blur to work properly...

--- code/ui/Tooltip.cs
+++ code/ui/Tooltip.cs
@@ -45,11 +45,11 @@ namespace Facepunch.CoreWars
 			NameLabel.Style.TextShadow = new ShadowList();
 			NameLabel.Style.TextShadow.Add( new Shadow()
 			{
-				Color = Color.Lerp( provider.Color, Color.Black, 0.3f ).WithAlpha( 0.3f ),
+				Color = Color.Lerp( provider.Color, Color.Black, 0.3f ).WithAlpha( 0.1f ),
 				OffsetX = 0f,
 				OffsetY = 0f,
-				Spread = -3f,
-				Blur = 8f
+				Spread = 0f,
+				Blur = 3f
 			} );
 			NameLabel.Style.FontColor = provider.Color;
 
